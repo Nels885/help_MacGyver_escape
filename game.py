3b374faaf5a40be4266de8377228b7ca2ddd43de
Nels885 <voirin.lionel@gmail.com>
2017-11-12 11:52:54 +0100

    game.py file code changes for pygame

--- game.py
+++ game.py
@@ -16,9 +16,35 @@ import labyrinth
 import character
 import object
 
+# Constantes
 SIZE_SPRITE = 40
 NB_SPRITE = 15
 NB_OBJECTS = 3
+
+# Pygame Initialize
+pygame.init()
+windows = pygame.display.set_mode((SIZE_SPRITE * NB_SPRITE, SIZE_SPRITE * NB_SPRITE))
+pygame.display.set_caption('Help MacGyver Escape')
+background = pygame.Surface(windows.get_size())
+background = background.convert()
+background.fill((0, 0, 0))
+
+# Sprites labyrinth
+sprite_wall = pygame.image.load("pictures/wall.png").convert()
+sprite_ground = pygame.image.load("pictures/ground.png").convert()
+
+# Sprite objects
+sprite_needle = pygame.image.load("pictures/needle.png").convert_alpha()
+sprite_tube = pygame.image.load("pictures/tube.png").convert_alpha()
+sprite_ether = pygame.image.load("pictures/ether.png").convert_alpha()
+
+# Sprite characters
+sprite_guardian = pygame.image.load("pictures/murdoc-32.png").convert_alpha()
+sprite_mgyver = pygame.image.load("pictures/macgyver-32-43.png").convert_alpha()
+
+# Dictionary of sprites
+DICT_SPRITES = {" ": sprite_ground, "0": sprite_wall, "F": sprite_guardian, "M": sprite_mgyver,
+                "N": sprite_needle, "T": sprite_tube, "E": sprite_ether}
 
 # Class initializes
 cha = character.Character()
@@ -48,26 +74,27 @@ class Game:
         lab.add_sprite(pos_tube, "T")
         lab.add_sprite(pos_ether, "E")
         lab.add_sprite(pos_mgyver, cha.sprite_mgyver)
+        self.structure = lab.structure_laby
 
-    def display(self, sprite_lab, size_sprite):
+    def display(self):
         """
         ## Load all sprites of the game ##
-            :param sprite_lab: dictionary of sprites for the window of pygame
+            :param dict_sprites: dictionary of sprites for the window of pygame
             :param size_sprite: size des sprites
             :return: add all sprites to the window of pygame
         """
-        for y in range(15):
+        for y in range(NB_SPRITE):
             x = 0
             line_modif = ""
             line = lab.structure_laby[y]
             for value in line:
-                for key in sprite_lab.keys():
+                for key in DICT_SPRITES.keys():
                     if value == key:
-                        windows.blit(sprite_lab[key], (size_sprite * x, size_sprite * y))
+                        windows.blit(DICT_SPRITES[key], (SIZE_SPRITE * x, SIZE_SPRITE * y))
                 line_modif += value
                 x += 1
 
-    def end_game(self, structure):
+    def end_game(self):
         """
         ## Check end game ##
         Check if you are at the exit of the
@@ -75,7 +102,7 @@ class Game:
             :param structure: attribute of labyrinth class
             :return: end game is True or False
         """
-        if structure[cha.pos_mgyver[1]][cha.pos_mgyver[0]] == "F":
+        if self.structure[cha.pos_mgyver[1]][cha.pos_mgyver[0]] == "F":
             if cha.objects == NB_OBJECTS:
                 self.end_text = self.WON_TEXT
             else:
@@ -117,42 +144,6 @@ def main():
     """
     Main instruction for to run the game
     """
-    end = True
-    while end:
-        pygame.time.delay(100)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game.end_text = game.QUIT_TEXT
-                end = False
-            if event.type == KEYDOWN:
-                if event.key == K_UP:
-                    cha.move("up")
-                if event.key == K_DOWN:
-                    cha.move("down")
-                if event.key == K_LEFT:
-                    cha.move("left")
-                if event.key == K_RIGHT:
-                    cha.move("right")
-                end = game.end_game(lab.structure_laby)
-
-                # check position if wall or objects
-                if not cha.check_position(lab.structure_laby):
-                    log.info("Erreur de direction !!!")
-                else:
-                    lab.move_sprite(cha.pos_mgyver, cha.sprite_mgyver)
-                    # display info messages or debug messages
-                    log.info("Position MacGyver (x, y): {}, {}".format(int(cha.x_position), int(cha.y_position)))
-                    log.info("Objets disponible: {}".format(int(cha.objects)))
-                    for i in range(15):
-                        log.debug(lab.structure_laby[i])
-
-        # display of the Labyrinth
-        game.display(sprite_lab, SIZE_SPRITE)
-        pygame.display.flip()
-    game.end_screen()
-
-
-if __name__ == "__main__":
     args = parse_arguments()
     if args.debug:
         log.basicConfig(level=log.DEBUG)
@@ -161,27 +152,47 @@ if __name__ == "__main__":
     try:
         laby_file = args.file
         game = Game(laby_file)
+        end = True
+        while end:
+            pygame.time.delay(100)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game.end_text = game.QUIT_TEXT
+                    end = False
+                if event.type == KEYDOWN:
+                    if event.key == K_UP:
+                        cha.move("up")
+                    if event.key == K_DOWN:
+                        cha.move("down")
+                    if event.key == K_LEFT:
+                        cha.move("left")
+                    if event.key == K_RIGHT:
+                        cha.move("right")
+                    end = game.end_game()
 
-        # Pygame Initialize
-        pygame.init()
-        windows = pygame.display.set_mode((SIZE_SPRITE * NB_SPRITE, SIZE_SPRITE * NB_SPRITE))
-        pygame.display.set_caption('Help MacGyver Escape')
-        background = pygame.Surface(windows.get_size())
-        background = background.convert()
-        background.fill((0, 0, 0))
-        SPRITE_WALL = pygame.image.load("pictures/wall.png").convert()
-        SPRITE_GROUND = pygame.image.load("pictures/ground.png").convert()
-        SPRITE_NEEDLE = pygame.image.load("pictures/needle.png").convert_alpha()
-        SPRITE_TUBE = pygame.image.load("pictures/tube.png").convert_alpha()
-        SPRITE_ETHER = pygame.image.load("pictures/ether.png").convert_alpha()
-        SPRITE_GUARDIAN = pygame.image.load("pictures/murdoc-32.png").convert_alpha()
-        SPRITE_MGYVER = pygame.image.load("pictures/macgyver-32-43.png").convert_alpha()
+                    # check position if wall or objects
+                    if not cha.check_position(lab.structure_laby):
+                        log.info("Erreur de direction !!!")
+                    else:
+                        lab.move_sprite(cha.pos_mgyver, cha.sprite_mgyver)
+                        # display info messages or debug messages
+                        log.info("Position MacGyver (x, y): {}, {}".format(int(cha.x_position), int(cha.y_position)))
+                        log.info("Objets disponible: {}".format(int(cha.objects)))
+                        for i in range(15):
+                            log.debug(lab.structure_laby[i])
 
-        sprite_lab = {" ": SPRITE_GROUND, "0": SPRITE_WALL, "F": SPRITE_GUARDIAN, "M": SPRITE_MGYVER,
-                      "N": SPRITE_NEEDLE, "T": SPRITE_TUBE, "E": SPRITE_ETHER}
-        main()
+            # display of the Labyrinth
+            game.display()
+            pygame.display.flip()
+        game.end_screen()
+
     # return an error when the name of file is incorrect
     except FileNotFoundError as err:
         log.error(err)
+
+
+if __name__ == "__main__":
+    main()
+
 
 
